2426f98c0e1562d62d81b75d39620dc50dd84a72
Guilherme Viana <guilhermehviana01@gmail.com>
2021-09-05 01:16:41 -0300

    Add transitions to header

--- src/Components/NavBarContent.jsx
+++ src/Components/NavBarContent.jsx
@@ -4,22 +4,21 @@ import logo from '../logo.svg'
 import '../Style/NavBar.css'
 import '../App.css'
 
-
 export default class NavBarContent extends Component {
   render() {
     return (
         <Navbar fixed="top" className="navbar-container" expand="lg" variant="light">
           <img src={logo} className="App-logo" alt="React logo" />
           <Container>
-            <Navbar.Brand>
+            <Navbar.Brand className="welcome-text">
               Welcome to my portfolio!
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto" >
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#projects">Projects</Nav.Link>
-        <Nav.Link href="#contact">Contact</Nav.Link>
+        <Nav.Link href="#home" className="nav-link"><span>Home</span></Nav.Link>
+        <Nav.Link href="#projects" className="nav-link"><span>Projects</span></Nav.Link>
+        <Nav.Link href="#contact" className="nav-link"><span>Contact</span></Nav.Link>
         {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
