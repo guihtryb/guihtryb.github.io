b80954bc3c6792b6e52f480b1f1d7d8faae3a43e
Guilherme Viana <guilhermehviana01@gmail.com>
2022-10-25 18:32:59 -0300

    refactor: refactor header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
@@ -10,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/sobre" element={<About />} />
         <Route path="/projetos" element={<Projects />} />
         <Route path="*" element={<NotFound />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, Navigate, useLocation } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
   const { pathname } = useLocation();
+
+  if (pathname === '/') return <Navigate to="/home" />;
 
   const pages = ['Home', 'Sobre', 'Projetos'];
   const currRoute = (page) => pathname === `/${page.toLowerCase()}`;
@@ -13,7 +15,7 @@ export default function Header() {
       <nav className="menu-container">
         <ul className="menu-list">
           {
-            pages.map((page) => (page !== 'Home' ? (
+            pages.map((page) => (
               <li
                 key={page}
                 style={{
@@ -23,18 +25,8 @@ export default function Header() {
               >
                 <Link to={`/${page.toLowerCase()}`} style={{ color: currRoute(page) ? '#52B788' : '#FFF' }}>{page}</Link>
               </li>
-            ) : (
-              <li
-                key={page}
-                style={{
-                  borderBottom: pathname === '/' ? '3.5px solid #52B788' : '0',
-                  paddingBottom: pathname === '/' ? '0' : '3.5px solid #52B788',
-                }}
-              >
-                <Link to="/" style={{ color: pathname === '/' ? '#52B788' : '#FFF' }}>{page}</Link>
-              </li>
-            )))
-          }
+            ))
+        }
         </ul>
       </nav>
     </header>
