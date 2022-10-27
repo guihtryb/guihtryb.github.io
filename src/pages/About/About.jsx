d7e1afe30f2787f3975c4546133c10d925e73acf
Guilherme Viana <guilhermehviana01@gmail.com>
2022-10-27 20:28:29 -0300

    feat: add  skills to about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,44 +2,110 @@ import React from 'react';
 import ContactModal from '../../components/ContactModal/ContactModal';
 import Header from '../../components/Header/Header';
 import './About.css';
+import skills from './skills';
 
 export default function About() {
   return (
     <div className="about-page">
       <Header />
       <main className="main-about-bg">
+        <div className="img-about-bg" alt="background decoration" />
         <div className="main-about-container">
           <h1>Olá,</h1>
-          <h2>Meu nome é Guilherme Viana!</h2>
+          <h2>
+            Meu nome é
+            {' '}
+            <span>
+              {' '}
+              Guilherme Viana!
+            </span>
+
+          </h2>
           <p>
-            Sou apaixonado por tecnologia e seu potencial
-            de transformar positivamente a vida das
+            Sou
+            {' '}
+            <span>apaixonado</span>
+            {' '}
+            por
+            {' '}
+            <span>tecnologia</span>
+            {' '}
+            e seu potencial de
+            {' '}
+            <span>
+              transformar
+              positivamente
+            </span>
+            {' '}
+            a vida das
             pessoas.
           </p>
           <p>
-            Possuo foco em desenvolvimento Front-End e
-            tenho aprofundado minhas habilidades com ReactJS.
+            Possuo foco em desenvolvimento
+            {' '}
+            <span>
+              Front-End
+            </span>
+            {' '}
+            e tenho aprofundado minhas habilidades com
+            {' '}
+            <span> ReactJS</span>
+            .
           </p>
           <p>
-            Iniciei meus estudos em desenvolvimento e lógica de programação em 2017
-            e aprofundo meus conhecimentos em
-            desenvolvimento Web desde 2021, quando iniciei meu curso de desenvolvimento Web
-            Full-Stack na
+            Iniciei meus estudos em desenvolvimento e
             {' '}
-            <a href="https://www.betrybe.com/">Trybe</a>
+            <span>lógica de programação</span>
+            {' '}
+            em 2017 e aprofundo meus conhecimentos em
+            desenvolvimento Web desde 2021, quando iniciei meu curso de
+            {' '}
+            <span>
+              desenvolvimento Web Full-Stack
+            </span>
+            {' '}
+            na
+            {' '}
+            <a href="https://www.betrybe.com/">
+              <span>
+                Trybe
+              </span>
+            </a>
             {' '}
             ,
             e tenho
-            desenvolvido projetos guiados por esse potencial desde então.
+            desenvolvido projetos guiados por esse potencial de transformação desde então.
           </p>
           <p>
-            A capacidade de investigação, determinação e
-            resiliência, são habilidades que carrego comigo para a superação de qualquer desafio!
+            A capacidade de
+            {' '}
+            <span>
+              {' '}
+              investigação, determinação e
+              resiliência
+            </span>
+            , são habilidades que carrego comigo para a superação de
+            {' '}
+            <span>
+              qualquer desafio!
+            </span>
           </p>
         </div>
         <section className="skills-section-bg">
-          <div className="skills-container">
-            Skills
+          <h3 className="skill-subtitle">Skills</h3>
+          <div
+            className="skills-container"
+          >
+            {
+                skills.map((skill) => (
+                  <div className="skill" key={skill}>
+                    <div className="skill-icon-bg">
+                      <img src={skill.icon} alt={`${skill.name} icon`} />
+                    </div>
+                    <h4>{skill.name}</h4>
+                  </div>
+                ))
+              }
           </div>
         </section>
       </main>
